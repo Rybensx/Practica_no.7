de6e2909ce5cf0b35e9529105d4a7f76c792ee5a
Rybensx <anderson_arf@hotmail.es>
2023-05-20 13:00:40 -0600

    practica07_inicio

---
+++ bd.sql
@@ -0,0 +1,11 @@
+create table estudiantes (
+    alumno_id serial not null,
+    primer_nombre varchar(50) not null,
+    segundo_nombre varchar(50),
+    primer_apellido varchar(50) not null,
+    segundo_apellido varchar(50),
+    alumno_nacimiento date not null,
+    alumno_tel VARCHAR not null,
+    alumno_email VARCHAR(100),
+    primary key (alumno_id)
+)
---
+++ modelos/estudiantes.php
@@ -0,0 +1,31 @@
+<?php
+require 'Conexion.php';
+
+class Producto extends Conexion{
+    public $alumno_id;
+    public $primer_nombre;
+    public $segundo_nombre;
+    public $primer_apellido;
+    public $segundo_apellido;
+    public $alumno_nacimiento;
+    public $alumno_tel;
+    public $alumno_email;
+
+    public function __construct($args = [] )
+    {
+        $this->alumno_id = $args['alumno_id'] ?? null;
+        $this->primer_nombre = $args['primer_nombre'] ?? '';
+        $this->segundo_nombre = $args['segundo_nombre'] ?? '';
+        $this->primer_apellido = $args['primer_apellido'] ?? '';
+        $this->segundo_apellido = $args['segundo_apellido'] ?? '';
+        $this->alumno_nacimiento = $args['alumno_nacimiento'] ?? '';
+        $this->alumno_tel = $args['alumno_tel'] ?? '';
+        $this->alumno_email = $args['alumno_email'] ?? '';
+    }
+
+    public function guardar(){
+        $sql = "INSERT INTO estudiantes(primer_nombre, segundo_nombre, primer_apellido, segundo_apellido, alumno_nacimiento, alumno_tel, alumno_email) values('$this->primer_nombre','$this->segundo_nombre','$this->primer_apellido','$this->segundo_apellido','$this->alumno_nacimiento','$this->alumno_tel','$this->alumno_email')";
+        $resultado = self::ejecutar($sql);
+        return $resultado;
+    }
+}
